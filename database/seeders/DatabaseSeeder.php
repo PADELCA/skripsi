716061df403e2985a415d2c470d09f2ff3b068e7
Fadel Abdul Fiqri <email_anda@gmail.com>
2026-06-19 00:51:11 +0700

    Menyimpan perubahan lokal agar bisa push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Fadel Abdul Fiqri',
+            'email' => 'fadelabdul19@gmail.com',
         ]);
     }
 }
